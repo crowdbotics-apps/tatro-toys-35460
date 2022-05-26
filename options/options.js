2af226c0387c6c57ef8594d45360361f08a90a3e
Crowdbotics <team@crowdbotics.com>
2022-05-26 20:25:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tatro_toys_35460",
+  url: "https://tatro_toys_35460.botics.co",
+  api: "https://tatro_toys_35460.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
